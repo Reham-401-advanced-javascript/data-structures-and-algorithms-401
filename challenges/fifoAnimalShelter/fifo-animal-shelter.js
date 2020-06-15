c2eb743d9e20b850e8ec815b55d5526618f81520
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-15 20:33:43 +0300

    done

--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,63 +1,52 @@
 'use strict';
+'use strict';
+
 class AnimalShelter {
   constructor() {
-    this.storage=[];
+    this.storage = [];
+    this.front = null;
   }
   enqueue(animal) {
-   
-    if(animal){
-      if(animal.type === 'dog' || animal.type === 'cat'){
-        this.storage.push(animal);
-      }else{
-        return null;
-      }
-       
-    }}
-  dequeue(pref){
-    if(pref){
-      while(this.storage.length != 0){
-        
-        if(this.peek().name === pref.name && this.peek().type === pref.type){
-          return this.peek();
-        }else{
-          this.storage.shift();
-        }
-      }
+    if ((animal === 'dog') || (animal === 'cat')) {
+      this.storage.push(animal);
+      this.front = this.storage[0];
+    }
+    else {
       return null;
     }
   }
-  peek(){
-    return this.storage[0];
+  dequeue(pref) {
+    let counter = 0;
+    if ((pref === 'dog') || (pref === 'cat')) {
+      let newAnimal = [];
+      for (let i = 0; i < this.storage.length; i++) {
+        newAnimal.push(this.storage[i]);
+        if ((this.storage[i] === pref) && (counter === 0)) {
+          newAnimal.pop();
+          counter++;
+        }
+      }
+      this.storage = newAnimal;
+      this.front = this.storage[0];
+      return  pref;
+    } else {
+      return null;
+    }
   }
 }
 
-// class Queue {
-//   constructor() {
-//     this.storage = [];
-//   }
-//   enqueue(item) {
-//     this.storage.push(item);
-//     //this.storage.unshift(item)
-//   }
-//   dequeue() {
-//     return this.storage.shift();
-//     // return this.storage.pop()
-//   }
-
-//   peek() {
-//     return this.storage[0];
-//   }
-//   
-// }
-
 module.exports = AnimalShelter;
-let shelter=new AnimalShelter();
-shelter.enqueue({type : 'cat' , name : 'caty'});
-shelter.enqueue({type : 'dog' , name : 'loly'});
-shelter.enqueue({type : 'dog' , name : 'black'});
-shelter.dequeue({type : 'dog' , name : 'loly'});
+// let shelter = new AnimalShelter();
+// shelter.enqueue('cat');
+// shelter.enqueue('dog');
+// shelter.enqueue('dog');
+// shelter.enqueue('dg');
+
+// shelter.dequeue('cat');
+
+// console.log('ffff',shelter);
 
 
 
 
-console.log('ffff',shelter);
+
